1e7fee7f8b8c453af791b69a647e15edaa987c16
David Linhart <DaveL@dhicorp.com>
2018-09-18 14:00:39 -0600

    Sub nav components

---
+++ projects/gps-components/src/lib/app-sub-nav/customer-contact/customer-contact.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, Input } from "@angular/core";
+import { MatDialog } from "@angular/material";
+import { NavOverlayComponent } from "../../nav-overlay/nav-overlay.component";
+
+@Component({
+  selector: "gps-customer-contact",
+  templateUrl: "./customer-contact.component.html",
+  styleUrls: ["./customer-contact.component.scss"]
+})
+export class CustomerContactComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+
+  openDialog() {}
+}
--- projects/gps-components/src/lib/gps-components.module.ts
+++ projects/gps-components/src/lib/gps-components.module.ts
@@ -1,18 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { GpsComponentsComponent } from './gps-components.component';
-import { TableComponent } from './tables/table.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { LibMaterialModule } from './lib-material.module';
-import { AppMainNav } from './app-main-nav/app-main-nav.component';
-import { RemindersComponent } from './app-main-nav/reminders/reminders.component';
-import { AssignmentsComponent } from './app-main-nav/assignments/assignments.component';
-import { ShellSearch } from './app-main-nav/shell-search/shell-search.component';
-import { A11yModule } from '@angular/cdk/a11y';
-import { CommonModule } from '@angular/common';
-import { NavOverlayComponent } from './nav-overlay/nav-overlay.component';
-import { AppsSidebarComponent } from './app-main-nav/apps-sidebar/apps-sidebar.component';
-import { DialogModule } from './dialog/dialog.module';
+import { CustomerNotesComponent } from "./app-sub-nav/customer-notes/customer-notes.component";
+import { CustomerContactComponent } from "./app-sub-nav/customer-contact/customer-contact.component";
+import { AppSubNav } from "./app-sub-nav/app-sub-nav.component";
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { GpsComponentsComponent } from "./gps-components.component";
+import { TableComponent } from "./tables/table.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { LibMaterialModule } from "./lib-material.module";
+import { AppMainNav } from "./app-main-nav/app-main-nav.component";
+import { RemindersComponent } from "./app-main-nav/reminders/reminders.component";
+import { AssignmentsComponent } from "./app-main-nav/assignments/assignments.component";
+import { ShellSearch } from "./app-main-nav/shell-search/shell-search.component";
+import { A11yModule } from "@angular/cdk/a11y";
+import { CommonModule } from "@angular/common";
+import { NavOverlayComponent } from "./nav-overlay/nav-overlay.component";
+import { AppsSidebarComponent } from "./app-main-nav/apps-sidebar/apps-sidebar.component";
+import { DialogModule } from "./dialog/dialog.module";
 
 @NgModule({
   imports: [
@@ -21,25 +24,29 @@ import { DialogModule } from './dialog/dialog.module';
     BrowserModule,
     BrowserAnimationsModule,
     LibMaterialModule,
-    DialogModule,
+    DialogModule
   ],
   declarations: [
     GpsComponentsComponent,
     AppMainNav,
+    AppSubNav,
     RemindersComponent,
     ShellSearch,
     AssignmentsComponent,
+    CustomerNotesComponent,
+    CustomerContactComponent,
     TableComponent,
     NavOverlayComponent,
-    AppsSidebarComponent,
+    AppsSidebarComponent
   ],
   entryComponents: [NavOverlayComponent],
   exports: [
     GpsComponentsComponent,
     AppMainNav,
+    AppSubNav,
     TableComponent,
     AppsSidebarComponent,
-    DialogModule,
-  ],
+    DialogModule
+  ]
 })
 export class GpsComponentsModule {}
